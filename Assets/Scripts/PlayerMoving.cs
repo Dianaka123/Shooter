c9259c0bbea700417be67aa3c5d782a1c8677895
Di <diana.aristova.99@gmail.com>
2020-10-14 16:26:55 +0300

    Add rotation without limit and 2 screen

--- Assets/Scripts/PlayerMoving.cs
+++ Assets/Scripts/PlayerMoving.cs
@@ -1,25 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Serialization;
 
 public class PlayerMoving : MonoBehaviour
 {
-    [FormerlySerializedAs("Speed")] 
-    public float speed = 10;
+    [FormerlySerializedAs("Speed")] public float speed = 10;
 
-    public float rotationSpeed = 100.0f;
-    
     // Update is called once per frame
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxisRaw("Horizontal");
         float moveVertical = Input.GetAxisRaw("Vertical");
- 
+        
+        
+        
         var movement = new Vector3(moveHorizontal, 0, moveVertical );
- 
         movement = movement * speed * Time.deltaTime;
- 
-        transform.Translate(movement);
+        transform.position += movement;
+       
     }
 }
---
+++ Assets/WeaponMoving.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponMoving : MonoBehaviour
+{
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+    
+    public float rotationSpeed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        yaw += rotationSpeed * Input.GetAxis("Mouse X");
+        pitch = Input.GetAxis("Mouse Y") * 10;
+        Debug.Log(pitch);
+        transform.Rotate(pitch, yaw , 0);
+        
+    }
+}
