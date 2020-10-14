b1bb6172775a2189ccd998c9387e89e5ba490ff1
Di <diana.aristova.99@gmail.com>
2020-10-14 14:59:18 +0300

    Add player moving.

---
+++ Assets/Scripts/PlayerMoving.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerMoving : MonoBehaviour
+{
+    [FormerlySerializedAs("Speed")] 
+    public float speed = 10;
+
+    public float rotationSpeed = 100.0f;
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+ 
+        var movement = new Vector3(moveHorizontal, 0, moveVertical );
+ 
+        movement = movement * speed * Time.deltaTime;
+ 
+        transform.Translate(movement);
+    }
+}
