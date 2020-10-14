ee1c775bc7a652e511b29f9c9dba8d3c75cdee73
Di <diana.aristova.99@gmail.com>
2020-10-14 17:43:32 +0300

    Add limit for rotation

--- Assets/WeaponMoving.cs
+++ Assets/WeaponMoving.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 
 public class WeaponMoving : MonoBehaviour
 {
+    public Transform CameraTransform;
+    
     private float yaw = 0.0f;
     private float pitch = 0.0f;
     
-    public float rotationSpeed = 10.0f;
+    //Y limit
+    public float yMaxLimit = 45.0f;
+    public float yMinLimit = -45.0f;
+    float yRotCounter = 0.0f;
+
+    //X limit
+    public float xMaxLimit = 45.0f;
+    public float xMinLimit = -45.0f;
+    float xRotCounter = 0.0f;
+    
+    public float xMoveThreshold = 1000.0f;
+    public float yMoveThreshold = 1000.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,10 +32,16 @@ public class WeaponMoving : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        yaw += rotationSpeed * Input.GetAxis("Mouse X");
-        pitch = Input.GetAxis("Mouse Y") * 10;
-        Debug.Log(pitch);
-        transform.Rotate(pitch, yaw , 0);
+        //Get X value and limit it
+        xRotCounter += Input.GetAxis("Mouse X") * xMoveThreshold * Time.deltaTime;
+        xRotCounter = Mathf.Clamp(xRotCounter, xMinLimit, xMaxLimit);
         
+        //Get Y value and limit it
+        yRotCounter += Input.GetAxis("Mouse Y") * yMoveThreshold * Time.deltaTime;
+        yRotCounter = Mathf.Clamp(yRotCounter, yMinLimit, yMaxLimit);
+        //xRotCounter = xRotCounter % 360;//Optional
+        //transform.localEulerAngles = new Vector3(-yRotCounter, xRotCounter, 0);
+        transform.localEulerAngles = new Vector3(90, xRotCounter, 0);
+        CameraTransform.localEulerAngles = new Vector3(-yRotCounter + 90, xRotCounter, 0);
     }
 }
