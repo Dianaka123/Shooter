e2001a0755c2e83d97f12c51b3ea71cbe291c8b2
Di <diana.aristova.99@gmail.com>
2020-10-14 18:05:48 +0300

    Add moving for two players

--- Assets/Scripts/PlayerMoving.cs
+++ Assets/Scripts/PlayerMoving.cs
@@ -1,24 +1,52 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Serialization;
+
+public enum PlayersIndex
+{
+    First = 1,
+    Second
+}
 
 public class PlayerMoving : MonoBehaviour
 {
     [FormerlySerializedAs("Speed")] public float speed = 10;
 
+    [FormerlySerializedAs("PlayersIndex")] public PlayersIndex playersIndex = PlayersIndex.First;
+
     // Update is called once per frame
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxisRaw("Horizontal");
-        float moveVertical = Input.GetAxisRaw("Vertical");
-        
-        
-        
-        var movement = new Vector3(moveHorizontal, 0, moveVertical );
-        movement = movement * speed * Time.deltaTime;
-        transform.position += movement;
-       
+        var moveHorizontal = Input.GetAxisRaw("Horizontal");
+        var moveVertical = Input.GetAxisRaw("Vertical");
+
+        switch (playersIndex)
+        {
+            case PlayersIndex.First:
+                ArrowMoving();
+                return;
+            case PlayersIndex.Second:
+                transform.position = WasdMoving();
+                return;
+        }
+    }
+
+    private void ArrowMoving()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) transform.position += Vector3.left * speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.RightArrow)) transform.position += Vector3.right * speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.UpArrow)) transform.position += Vector3.forward * speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.DownArrow)) transform.position += -Vector3.forward * speed * Time.deltaTime;
+    }
+
+    private Vector3 WasdMoving()
+    {
+        var pos = transform.position;
+
+        if (Input.GetKey("w")) pos.z += speed * Time.deltaTime;
+        if (Input.GetKey("s")) pos.z -= speed * Time.deltaTime;
+        if (Input.GetKey("d")) pos.x += speed * Time.deltaTime;
+        if (Input.GetKey("a")) pos.x -= speed * Time.deltaTime;
+
+        return pos;
     }
 }
--- Assets/WeaponMoving.cs
+++ Assets/WeaponMoving.cs
@@ -1,46 +1,35 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class WeaponMoving : MonoBehaviour
 {
     public Transform CameraTransform;
-    
-    private float yaw = 0.0f;
-    private float pitch = 0.0f;
-    
+
     //Y limit
     public float yMaxLimit = 45.0f;
     public float yMinLimit = -45.0f;
-    float yRotCounter = 0.0f;
 
     //X limit
     public float xMaxLimit = 45.0f;
     public float xMinLimit = -45.0f;
-    float xRotCounter = 0.0f;
-    
+
     public float xMoveThreshold = 1000.0f;
     public float yMoveThreshold = 1000.0f;
+    private float pitch = 0.0f;
+    private float xRotCounter;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float yaw = 0.0f;
+    private float yRotCounter;
 
-    // Update is called once per frame
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         //Get X value and limit it
         xRotCounter += Input.GetAxis("Mouse X") * xMoveThreshold * Time.deltaTime;
         xRotCounter = Mathf.Clamp(xRotCounter, xMinLimit, xMaxLimit);
-        
+
         //Get Y value and limit it
         yRotCounter += Input.GetAxis("Mouse Y") * yMoveThreshold * Time.deltaTime;
         yRotCounter = Mathf.Clamp(yRotCounter, yMinLimit, yMaxLimit);
-        //xRotCounter = xRotCounter % 360;//Optional
-        //transform.localEulerAngles = new Vector3(-yRotCounter, xRotCounter, 0);
+
         transform.localEulerAngles = new Vector3(90, xRotCounter, 0);
         CameraTransform.localEulerAngles = new Vector3(-yRotCounter + 90, xRotCounter, 0);
     }
